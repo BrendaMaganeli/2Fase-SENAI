e802afd038ef8371169ccdd297dbe2c96c378bc6
BrendaMaganeli <brenda_casimiro@estudante.sesisenai.org.br>
2024-08-23 17:16:13 -0300

    Criação do Primeiro projeto com react bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnívoros</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat odio accusamus eum maxime! Esse reiciendis explicabo, voluptates neque nemo fuga consectetur eaque quia libero temporibus iure tempore eligendi et repudiandae?</p>
+    </>
+  )
+}
+
+export default App
